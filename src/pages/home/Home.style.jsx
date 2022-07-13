045b63d7e5555333e0f9ef2ab5d75e989210344b
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2022-07-13 23:35:09 +0300

    Update Home.style.jsx

---
+++ src/pages/home/Home.style.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const ImgDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 1.5rem;
+`;
+
+export const HomeImg = styled.img`
+  width: 80%;
+  max-width: 750px;
+`;
+
+export const HeaderText = styled.h1`
+  font-size: 2.5rem;
+  color: red;
+  text-align: center;
+`;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
