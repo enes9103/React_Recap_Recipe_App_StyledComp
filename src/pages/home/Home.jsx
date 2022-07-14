7908d2b97e9f9af60d4ede92d299f699b456f5fe
EnesUnlu <enes9103@gmail.com>
2022-07-14 20:29:37 +0300

    data fetching

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,28 @@
-import HomeStyle from './Home.style.jsx'
+import HomeStyle from './Home.style'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Home = () => {
+  const [query, setQuery] = useState('egg')
+  const [selectedMeal, setSelectedMeal] = useState('breakfast')
+
+  const APP_ID ="f6edb9f6";
+  const APP_KEY ="4ab957a75da653a00a88eb87864b8139";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`
+  const getData = async() => {
+    const { data } = await axios.get(url)
+    console.log(data.hits)
+  };
+
+  useEffect(() => {
+  getData();
+  }, [])
+
   return (
     <div>
-      Home
+      
     </div>
   )
 }
