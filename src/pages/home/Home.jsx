50d8ed8d86f50dedc9978e8fccb43e2dd04c5b89
EnesUnlu <enes9103@gmail.com>
2022-07-15 14:42:19 +0300

    cards comp. completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,34 +1,35 @@
-import HomeStyle, { HeaderText, HomeImg, ImgDiv } from "./Home.style";
-import axios from "axios";
-import { useState } from "react";
-import Header from "../../components/header/Header";
-import Cards from "../../components/cards/Cards";
-import homeSvg from "../../assets/home.svg";
+import { HeaderText, HomeImg, ImgDiv } from './Home.style';
+import axios from 'axios';
+import { useState } from 'react';
+import Header from '../../components/header/Header';
+import Cards from '../../components/cards/Cards';
+import homeSvg from '../../assets/home.svg';
 
 const Home = () => {
-  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
-  const [query, setQuery] = useState("egg");
+  const mealType = ['Breakfast', 'Lunch', 'Dinner', 'Snack', 'Teatime'];
+  const [query, setQuery] = useState('egg');
   const [selectedMeal, setSelectedMeal] = useState(mealType[0]);
   const [recipes, setRecipes] = useState(null);
 
-  const APP_ID = "f6edb9f6";
-  const APP_KEY = "4ab957a75da653a00a88eb87864b8139";
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
   const getData = async () => {
     if (query) {
       try {
         const { data } = await axios.get(url);
-        // console.log(data.hits)
         setRecipes(data.hits);
       } catch (error) {
         console.log(error);
       }
     } else {
-      alert("Please enter a valid query");
+      alert('Please Enter your meal');
     }
   };
 
+  // console.log(recipes);
   return (
     <div>
       <Header
@@ -42,13 +43,14 @@ const Home = () => {
           <HomeImg src={homeSvg} />
         </ImgDiv>
       )}
+
       {recipes?.length === 0 && (
-        <HeaderText>
-          No recipes found for {query}
-        </HeaderText>
+        <HeaderText>The Food can not be found</HeaderText>
       )}
+
       {recipes?.length > 0 && <Cards recipes={recipes} />}
     </div>
   );
 };
+
 export default Home;
